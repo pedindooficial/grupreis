870244a2ce46999c902387ff66b5ffe887753ff8
analista1996 <afmdaniel@hotmail.com>
2026-01-06 16:43:33 -0300

    .

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -71,7 +71,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Accept", "x-user-id"]
+    allowedHeaders: ["Content-Type", "Authorization", "Accept", "x-user-id", "x-user-email"]
   })
 );
 
